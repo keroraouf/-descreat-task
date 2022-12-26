bbd36bd678d1be70b43f21b3f2640cea7888b1f8
keroraouf <121442089+keroraouf@users.noreply.github.com>
2022-12-26 17:00:47 +0200

    Add files via upload

---
+++ project1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+         
+        { 
+                int z, i, k, x, y;
+
+                Console.Write("\n\n");
+                Console.Write("Find the prime numbers within a range of numbers:\n");
+                Console.Write("---------------------------------------------------");
+                Console.Write("\n\n");
+
+                Console.Write("Input starting number of range: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Input ending number of range : ");
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.Write("The prime numbers between {0} and {1} are : \n", x, y);
+
+                for (z = x; z <= y; z++)
+                {
+                    k = 0;
+
+                    for (i = 2; i <= z / 2; i++)
+                    {
+                        if (z % i == 0)
+                        {
+                            k++;
+                            break;
+                        }
+                    }
+
+                    if (k == 0 && z != 1)
+                        Console.Write("{0} ", z);
+                }
+                Console.Write("\n");
+                    Console.ReadLine();
+            }
+        }
+        
+
+            }
